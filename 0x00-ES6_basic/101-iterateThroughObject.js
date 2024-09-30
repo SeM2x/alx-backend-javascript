0a2fdb11f7d8c001a78751b3a62d5e7d93b83e3c
SeM2x <houssem135.302@gmail.com>
2024-09-30 15:39:57 +0100

    101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,7 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const employees = []
+  const employees = [];
   for (const employee of reportWithIterator) {
-    employees.push(employee)
+    employees.push(employee);
   }
-  return employees.join(" | ")
+  return employees.join(" | ");
 }

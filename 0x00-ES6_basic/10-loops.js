9070d47176e8b5a70a6d5eea81d87d971254e678
SeM2x <houssem135.302@gmail.com>
2024-09-30 10:18:38 +0100

    fix checker

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
   for (const value of array) {
-    const newValue = appendString + value;
-    array[array.indexOf(value)] = newValue;
+    newArray.push(appendString + value);
   }
 
-  return array;
+  return newArray;
 }

adefa225dec3284fade4b0fccf5a6d8e3a46c373
SeM2x <houssem135.302@gmail.com>
2024-10-01 19:46:16 +0100

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,20 +1,9 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-  const data = [];
-  uploadPhoto()
-    .then((response) => {
-      data.push(response.body);
-      createUser()
-        .then((response) => {
-          data.push(response.firstName, response.lastName);
-          console.log(data.join(' '));
-        })
-        .catch(() => {
-          console.log('Signup system offline');
-        });
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }

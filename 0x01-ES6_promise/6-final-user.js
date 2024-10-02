c78ec8f18574f356eb56159d80a3003c099bcd81
SeM2x <houssem135.302@gmail.com>
2024-10-02 15:30:20 +0100

    6. Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,6 +5,12 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const promise = Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
+  ]).then((values) => {
+    const res = values.map((value) => ({
+      value: value.value || value.reason,
+      status: value.status,
+    }));
+    return res;
+  });
   return promise;
 }

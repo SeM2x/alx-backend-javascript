3cbe5c03f2a4bb02deaa3b84e844075c6494ddad
SeM2x <houssem135.302@gmail.com>
2024-10-08 08:52:09 +0100

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return '';
+  if (!startString || typeof startString !== 'string') return '';
   return Array.from(set)
     .filter((str) => str.startsWith(startString))
     .map((str) => str.slice(startString.length))
